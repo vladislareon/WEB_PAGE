c6ec39c9390af277afcdfb92a9c18bf3d90aa954
Malkov Vladislav <87611631+vladislareon@users.noreply.github.com>
2022-06-27 02:53:09 +0300

    Create __init__.py

---
+++ parser/__init__.py
@@ -0,0 +1,5 @@
+from .parser import parse, get_matching_blocks
+from .grid import prepare_map_irreg
+from .diffbot import get_text_from_diffbot
+
+__all__ = ["parse", "prepare_map_irreg", "get_matching_blocks", "get_text_from_diffbot"]
